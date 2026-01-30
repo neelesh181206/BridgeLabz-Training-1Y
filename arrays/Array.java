b65bf7411f2dddaf8a5b0746798d4e9673064f17
neelesh181206 <neelesh181206@gmail.com>
2026-01-30 10:01:33 +0530

    first commit

---
+++ arrays/Array.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Array{
+public static void main(String[] args){
+	String[] arr = new String[4];
+	arr[0] = "4";
+	arr[1] = "3";
+	arr[2] = "3";
+	arr[3] = "2";
+	for (int i=0;i<5;i++){
+		System.out.println(arr[i]+ " ");
+	}
+}
+}
